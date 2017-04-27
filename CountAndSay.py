b5f0c187db2a6e136413f026eb40f0dadddd9ace
wenqliu <liuwenqiangcs@gmail.com>
2017-04-26 21:21:55 -0700

    no message

--- CountAndSay.py
+++ CountAndSay.py
@@ -1,19 +1,19 @@
 class Solution:
-    # @param {int} n the nth
-    # @return {string} the nth sequence
-    def countAndSay(self, n):
-        if n<=0:
-            return ''
-        if n==1:
-            return '1'
-        seq=self.countAndSay(n-1)
-        result_seq=''
-        index_seq=1
-        for i in range(len(seq)):
-          if i+1<len(seq) and seq[i]==seq[i+1]:
-            index_seq+=1
-          else:
-            result_seq+=''+str(index_seq)
-            result_seq+=''+str(seq[i])
-            index_seq=1
-        return result_seq
+    # @param A, B: Two string.
+    # @return: the length of the longest common substring.
+    def longestCommonSubstring(self, A, B):
+        # write your code here
+        if not (A and B):
+            return 0
+        lcs = 0
+        for i in range(len(A)):
+            for j in range(len(B)):
+                lcs_temp = 0
+                while (i + lcs_temp < len(A) and
+                       j + lcs_temp < len(B) and
+                       A[i + lcs_temp] == B[j + lcs_temp]):
+                    lcs_temp += 1
+                # update lcs
+                if lcs_temp > lcs:
+                    lcs = lcs_temp
+        return lcs
