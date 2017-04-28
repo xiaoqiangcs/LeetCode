c66ef8e0468d5e02b4d9f6bf26a243e327b27285
wenqliu <liuwenqiangcs@gmail.com>
2017-04-27 22:57:48 -0700

    no message

--- Kth_Largest_Element.py
+++ Kth_Largest_Element.py
@@ -1,19 +1,25 @@
-class Solution:
-    # @param A, B: Two string.
-    # @return: the length of the longest common substring.
-    def longestCommonSubstring(self, A, B):
-        # write your code here
-        if not (A and B):
-            return 0
-        lcs = 0
-        for i in range(len(A)):
-            for j in range(len(B)):
-                lcs_temp = 0
-                while (i + lcs_temp < len(A) and
-                       j + lcs_temp < len(B) and
-                       A[i + lcs_temp] == B[j + lcs_temp]):
-                    lcs_temp += 1
-                # update lcs
-                if lcs_temp > lcs:
-                    lcs = lcs_temp
-        return lcs
+class Solution(object):
+    # @param k & A a integer and an array
+    # @return ans a integer
+    def kthLargestElement(self,k, A):
+        if k==0 or len(A)==0:
+            return ''
+        kth=self.qsort(A,0,len(A)-1,k)
+        return kth
+    def qsort(self,Array,left,Right,kth):
+        print(Array)
+        if left>Right:
+            return Array[Right]
+        index=left
+        for i in range(left+1, Right+1):
+            if Array[i]>Array[left]:
+                index+=1
+                Array[index],Array[i]=Array[i],Array[index]
+        Array[index],Array[left]=Array[left],Array[index]
+        if index+1==kth:
+            return Array[index]
+        elif index+1>kth:
+            return(self.qsort(Array,left,index-1,kth))
+        else:
+            return(self.qsort(Array,index+1,Right,kth))
+print(Solution().kthLargestElement(10,[1,2,3,4,5,6,8,9,10,7]))
