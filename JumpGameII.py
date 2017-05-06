b02fb91ac16bd9c7684cc6ba75f66d7b5adc4c9e
wenqliu <liuwenqiangcs@gmail.com>
2017-05-06 16:51:40 -0700

    JumpGame II

--- JumpGameII.py
+++ JumpGameII.py
@@ -4,9 +4,18 @@ class Solution (object):
     def canJump(self, A):
         if len(A) < 0:
             return -1
+        start = 0
+        end = 0
+        jumps = 0
         fastlength = A[0]
-        for index in range(1, len(A)):
-            if index < fastlength and index+A[index] > fastlength:
-                fastlength = index+A[index]
-        return fastlength > len(A)-1
-print(Solution().canJump([3, 2, 1, 0, 4]))
+        while end < len(A)-1:
+            fastlength = end
+            for index in range(start, end+1):
+                if index+A[index] > fastlength:
+                    fastlength = index+A[index]
+            if end < fastlength:
+                jumps += 1
+                start = end+1
+                end = fastlength
+        return jumps
+print(Solution().canJump([2, 3, 1, 1, 4]))
