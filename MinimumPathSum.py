2925328a282ee9fe4e364b588474fcdea84d5a45
wenqliu <liuwenqiangcs@gmail.com>
2017-05-05 21:25:41 -0700

    no message

--- MinimumPathSum.py
+++ MinimumPathSum.py
@@ -1,17 +1,17 @@
-#A robot is located at the top-left corner of a m x n grid
-#Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
-#Note
-#You can only move either down or right at any point in time.
+# A robot is located at the top-left corner of a m x n grid
+# Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+# Note
+# You can only move either down or right at any point in time.
 class Solution(object):
     # @param m: An integer m denotes the size of a backpack
     # @param A: Given n items with size A[i]
     # @return: The maximum size
-    def _minimum_path_sum(self,triangle):
-        if len(triangle)<0:
+    def _minimum_path_sum(self, triangle):
+        if len(triangle) < 0:
             return -1
-        row=len(triangle)
-        collum=len(triangle[0])
-        dp=[1000 for __ in range(collum)]
+        row = len(triangle)
+        collum = len(triangle[0])
+        dp = [1000 for __ in range(collum)]
         dp[0]=0
         for index_row in range(row):
             dp[0]=dp[0]+triangle[index_row][0]
--- MinimumTotal.py
+++ MinimumTotal.py
@@ -4,6 +4,6 @@ class Solution(object):
         dp=triangle[-1]
         for index in range(n-2,-1,-1):
             for j in range(index+1):
-                dp[j]=triangle[index][j]+min(dp[j],dp[j+1])
+                dp[j] = triangle[index][j] + min(dp[j],dp[j+1])
         return dp[0]
 
