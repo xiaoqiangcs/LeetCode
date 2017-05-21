cd057ac9a1df6ecca3adc1e6d3c21a71c7a9c789
wenqliu <liuwenqiangcs@gmail.com>
2017-05-21 10:25:41 -0700

    no message

--- Search2Dmatrix.py
+++ Search2Dmatrix.py
@@ -22,36 +22,18 @@ class Solution(object):
         """
         if not matrix or not matrix[0]:
             return False
-        row_length = len(matrix)
-        column_length = len(matrix[0])
-        down, up = -1, row_length
-        while down + 1 < up:
-            mid = down + int((up - down) / 2)
-            if matrix[mid][column_length - 1] < target:
-                down = mid
+        row, colmu  = 0, len(matrix[0])-1
+        corrent = 0
+        while row < len(matrix) and colmu >= 0:
+            if matrix[row][colmu] == target:
+                corrent+=1
+                colmu-=1
+            elif matrix[row][colmu] < target:
+                row+=1
             else:
-                up = mid
-        column_target = down + 1
-        print(column_target)
-        if column_target >= row_length:
-            return False
-        left, right = -1, column_length
-        while left + 1 < right:
-            mid = left + int((right - left) / 2)
-            if matrix[column_target][mid] < target:
-                left = mid
-            else:
-                right = mid
-        row_target = left + 1
-        print(row_target)
-        if row_target >= column_length:
-            return False
-        if (matrix[column_target][row_target] == target):
-            return True
-        else:
-            return False
-
+                colmu-=1
+        return corrent
 if __name__ == '__main__':
     array = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 50]]
-    index = Solution().searchMatrix(array, 55)
+    index = Solution().searchMatrix(array, 3)
     print(index)
