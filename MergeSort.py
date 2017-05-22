57e494caf246e02b434a82be1ce29aafe24527a3
wenqliu <liuwenqiangcs@gmail.com>
2017-05-22 14:07:42 -0700

    no message

--- MergeSort.py
+++ MergeSort.py
@@ -1,27 +1,35 @@
 class Solution(object):
-    def mergeSort(self, alist):
-        if len(alist)<=1:
-            return alist
-        mid = int(len(alist) / 2)
-        left = self.mergeSort(alist[:mid])
-        right = self.mergeSort(alist[mid:])
-        return self.sort(left,right)
-    def sort(self, left, right):
-        length_A = len(left)-1
-        length_B = len(right)-1
-        index_A, index_B = 0, 0
-        result = []
-        while index_A<=length_A and index_B <= length_B:
-            if left[index_A] <= right[index_B]:
-                result.append(left[index_A])
-                index_A+=1
-            else:
-                result.append(right[index_B])
-                index_B+=1
-        result+=left[index_A:]
-        result+=right[index_B:]
-        return result
+    def findMedianSortedArrays(self, A, B):
+        length = len(A) + len(B)
+        median_index = int(length / 2)
+        if length % 2 == 1:
+            return (self.findKth(A, B, median_index+1))
+        else:
+            return ((self.findKth(A, B, median_index) + self.findKth(
+                A, B, median_index + 1)) / 2.0)
+
+    def findKth(self, A, B, k):
+        if len(A) == 0:
+            return B[k - 1]
+        if len(B) == 0:
+            return A[k - 1]
+        if k == 1:
+            return min(A[0], B[0])
+        mid_length = int(k / 2)
+        if len(A) >= mid_length:
+            mid_A = A[mid_length - 1]
+        else:
+            mid_A = None
+        if len(B) >= mid_length:
+            mid_B = B[mid_length - 1]
+        else:
+            mid_B = None
+        if mid_B is None or (mid_A and mid_A < mid_B):
+            return self.findKth(A[mid_length:], B, k - mid_length)
+        else:
+            return self.findKth(A, B[mid_length:], k - mid_length)
 if __name__ == '__main__':
-    array = [6, 5, 3, 1, 8, 7, 2, 4]
-    index = Solution().mergeSort(array)
+    nums1 = [1, 2]
+    nums2 = [3]
+    index = Solution().findMedianSortedArrays(nums1, nums2)
     print(index)
